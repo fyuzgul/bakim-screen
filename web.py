10cd58f77ebf668bb9301c7c54b789e443523358
Muhammed Fatih Yüzgül <mfyuzgul@cmkkablo.corp>
2024-10-09 10:03:10 +0300

    updated

--- web.py
+++ web.py
@@ -9,8 +9,8 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-username = "muhammedfthyzgl@gmail.com"
-password = "fplflbpsyemswkoo"
+username = "elektrikcmk@gmail.com"
+password = "tmvtpyqcotpbsfdk"
 mail_server = "imap.gmail.com"
 
 emails_info = []
@@ -26,7 +26,7 @@ def check_emails():
             mail.select("inbox")
 
             today = datetime.today().strftime("%d-%b-%Y")
-            status, messages = mail.search(None, f'(ON "{today}" BODY "Makine Elektrik Arıza - Bakım Personeli")')
+            status, messages = mail.search(None, f'(ON "{today}" BODY "Makine Elektrik Ariza - Bakim Personeli")')
 
             if status == "OK":
                 email_ids = messages[0].split()
