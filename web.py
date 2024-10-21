31b60ac9b2e402d73febff7290405d0fa88e23c4
Muhammed Fatih Yüzgül <mfyuzgul@cmkkablo.corp>
2024-10-21 17:05:29 +0300

    bakim fix

--- web.py
+++ web.py
@@ -5,12 +5,15 @@ from email.header import decode_header
 from datetime import datetime
 import time
 import pytz
+from gtts import gTTS
+import os
+from playsound import playsound
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-username = "elektrikcmk@gmail.com"
-password = "tmvtpyqcotpbsfdk"
+username = "mekanikbakimcmkkablo@gmail.com"
+password = "sbiktfagyinhzjgu"
 mail_server = "imap.gmail.com"
 
 emails_info = []
@@ -26,7 +29,7 @@ def check_emails():
             mail.select("inbox")
 
             today = datetime.today().strftime("%d-%b-%Y")
-            status, messages = mail.search(None, f'(ON "{today}" BODY "Makine Elektrik Ariza - Bakim Personeli")')
+            status, messages = mail.search(None, f'(ON "{today}" BODY "Makine Mekanik Ariza - Bakim Personeli")')
 
             if status == "OK":
                 email_ids = messages[0].split()
@@ -112,12 +115,36 @@ def remove_email_after_delay(ist_no, delay):
     if ist_no in arrived_emails:
         arrived_emails.pop(ist_no)
 
+
+
 def display_email(email_info, email_time, status):
     email_info["Zaman"] = email_time
+    ist_no = email_info.get("IST NO", "")
+    
     if status == "waiting":
-        waiting_emails[email_info["IST NO"]] = email_info
+        waiting_emails[ist_no] = email_info
+        play_voice_alert(f"{ist_no} için Mekanik arıza var. Bakım personeli bekleniyor")
+
     elif status == "arrived":
-        arrived_emails[email_info["IST NO"]] = email_info
+        arrived_emails[ist_no] = email_info
+
+def play_voice_alert(message):
+    try:
+        # Metni sese çevir
+        tts = gTTS(text=message, lang='tr')
+        
+        # Ses dosyasını kaydet
+        tts.save("alert.mp3")
+        
+        # Ses dosyasını çal
+        playsound("alert.mp3")
+        
+        # Ses dosyasını sil
+        os.remove("alert.mp3")
+    
+    except Exception as e:
+        print(f"Ses oynatmada hata: {e}")
+
 
 @app.route('/')
 def index():
